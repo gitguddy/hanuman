cce1c2a88cd9b56cf5dcd2b2ab92b33569c5a4a3
grover <loro2008@gmx.de>
2018-01-17 16:52:40 +0100

    ...

--- gogsignin.js
+++ gogsignin.js
@@ -22,6 +22,7 @@ function googlsignin(){
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
+    console.log("errorMessage");
     // The email of the user's account used.
     var email = error.email;
     // The firebase.auth.AuthCredential type that was used.
