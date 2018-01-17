3977a5e3417aadca8fb094eafc1bafa89b5c93ab
grover <loro2008@gmx.de>
2018-01-17 14:32:58 +0100

    added some comments

--- gogsignin.js
+++ gogsignin.js
@@ -1,17 +1,24 @@
 var provider = new firebase.auth.GoogleAuthProvider();
 var user;
 
+// check if user is logged in
 firebase.auth().onAuthStateChanged(function(user)){
   if (user) {
+    // hide login if usetr is already logged in
     document.getElementById('goglog').style.display = "none";
+    // display greeting
     document.getElementById('sigend').style.display = "block";
+    // fetch user name for greeting
     $("#greeting1").html("Hello, "+ user.displayName);
   }else{
+    // display login if user is not logged in
     document.getElementById('goglog').style.display = "block";
+    // hide greeting
     document.getElementById('signed').style.display = "none";
   }
 }
 
+// sign in wi9th google
 function googlsignin(){
   firebase.auth().signInWithPopup(provider).then(function(result) {
     // This gives you a Google Access Token. You can use it to access the Google API.
@@ -31,9 +38,12 @@ function googlsignin(){
   });
 }
 
+// sign out
 function googlsignout(){
   firebase.auth().signOut().then(function() {
   // Sign-out successful.
+  
+  // reload site
   location.reload();
   }).catch(function(error) {
   // An error happened.
