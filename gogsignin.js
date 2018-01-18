78e7104231666f1957ac5c91e7209ba9116db18b
Lorenz <loro2008@gmx.de>
2018-01-18 07:52:32 +0100

    tried fixing the js code

--- gogsignin.js
+++ gogsignin.js
@@ -18,7 +18,7 @@ myBtn1.addEventListener('click',function(){
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
-    console.log("errorMessage");
+    console.log(errorMessage);
     // The email of the user's account used.
     var email = error.email;
     // The firebase.auth.AuthCredential type that was used.
